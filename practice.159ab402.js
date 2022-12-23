4123015928c0f2c0410db826fec3b442b94a27a0
SunAndrea <114474679+SunAndrea@users.noreply.github.com>
2022-12-23 15:59:20 +0000

    Deploying to gh-pages from @ SunAndrea/goit-js-hw-08@30f2000d4b576011edaf816aa03c0e7d96933258 🚀

--- practice.159ab402.js
+++
@@ -1,2 +0,0 @@
-!function(){function o(o){return new Promise((function(n,c){console.log("Fetching data for ".concat(o)),setTimeout((function(){c("error")}),2e3)}))}console.log(o("Andrii")),o("Andrii").then((function(n){console.log(n),console.log(o("Andrii"))})).catch((function(o){console.log(o)}))}();
-//# sourceMappingURL=practice.159ab402.js.map
--- practice.31778aaf.js
+++
@@ -1,2 +0,0 @@
-function o(o){return new Promise(((e,n)=>{console.log(`Fetching data for ${o}`),setTimeout((()=>{n("error")}),2e3)}))}console.log(o("Andrii")),o("Andrii").then((e=>{console.log(e),console.log(o("Andrii"))})).catch((o=>{console.log(o)}));
-//# sourceMappingURL=practice.31778aaf.js.map
